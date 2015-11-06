23f346099a7e0b24d47bac6cb004b15ed3d082a9
jyturley <jyturley@gmail.com>
2015-11-05 17:16:55 -0800

    added brew related things to mac setup

--- setup.sh
+++ setup.sh
@@ -5,17 +5,39 @@ DIR=$DIR/
 BASH_PROFILE=""
 VIMRC=""
 GITCONFIG=""
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	echo "OSX"
-	echo $DIR
-	BASH_PROFILE=${DIR}.bash_profile
-	ls $BASH_PROFILE && ln -s $BASH_PROFILE ~/.bash_profile
-	
-	VIMRC=${DIR}.vimrc
-	ls $VIMRC && ln -s $VIMRC ~/.vimrc
-
-	GITCONFIG=${DIR}.gitconfig
-	ls $GITCONFIG && ln -s $GITCONFIG ~/.gitconfig
-
-	echo "Done!"
+if [[ "$OSTYPE" != "darwin"* ]]; then
+	echo "this script only runs on osx currently"
 fi
+
+echo "Setting up OSX environment..."
+echo "linking .bash_profile..."
+BASH_PROFILE=${DIR}.bash_profile
+ls $BASH_PROFILE && ln -s $BASH_PROFILE ~/.bash_profile
+
+echo "linking .vimrc file..."
+VIMRC=${DIR}.vimrc
+ls $VIMRC && ln -s $VIMRC ~/.vimrc
+
+echo "linking .gitconfig file..."
+GITCONFIG=${DIR}.gitconfig
+ls $GITCONFIG && ln -s $GITCONFIG ~/.gitconfig
+
+if test ! $(which brew); then
+	echo "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+brew install coreutils
+brew install findutils
+brew install bash
+
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+$PATH=$(brew --prefix coreutiles)/libexec/gnubin:$PATH
+
+brew cleanup
+
+echo "Done!"
+
